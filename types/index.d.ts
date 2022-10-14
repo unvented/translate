15d9df7fbd9e41064bf1fed86832008202a16641
Samuel Kopp <62482066+unvented@users.noreply.github.com>
2022-10-14 21:02:21 +0200

    feat: add types

---
+++ types/index.d.ts
@@ -0,0 +1,1 @@
+export declare const useTranslate: (locales: { [key: string]: object }, currentLocale: string, fallbackLocale: string) => (key: string, variables?: { [key: string]: string }) => string
